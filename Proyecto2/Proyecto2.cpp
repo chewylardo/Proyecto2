e656f35e5b8d760429e60f96eb76e08330a67eb4
chewylardo <127953611+chewylardo@users.noreply.github.com>
2023-06-06 12:07:24 -0400

    Mañana 1

--- Proyecto2/Proyecto2.cpp
+++ Proyecto2/Proyecto2.cpp
@@ -11,9 +11,20 @@ using namespace std;
 
 
 int main()
-{
+{   
+    fstream newfile;
     cout << "Hello World!\n";
     cout << "proyecto envio\n";
+
+   newfile.open("Guardans.txt",ios::in); 
+   if (newfile.is_open()){ 
+      string tp;
+      while(getline(newfile, tp)){ 
+         cout << tp << "\n"; 
+      }
+      newfile.close(); 
+   }
+
 }
 
 
